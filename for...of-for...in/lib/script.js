0daea91836254479e711ce7d6f4d9c5e0ba1a8e4
lawrencedovin <lawrence@qwikwire.com>
2020-08-28 17:00:13 -0400

    Updated for of and for in loops

--- for...of-for...in/lib/script.js
+++ for...of-for...in/lib/script.js
@@ -1,14 +1,31 @@
 // Values
-let arr = ['a', 'b', 'c', 'd'];
+// let arr = ['a', 'b', 'c', 'd'];
 
-for (let n of arr) {
-  // what will n be?
-  console.log(n);
+// for (let n of arr) {
+//   // what will n be?
+//   console.log(n);
+// }
+
+// // Index
+
+// for (let n in arr) {
+//   // what will n be?
+//   console.log(n);
+// }
+
+/////////////////////////////////////////////////////////
+
+const colors = ['red', 'teal', 'cyan', 'yellow'];
+
+for(color of colors){
+  console.log(color);
 }
 
-// Index
+const Pogi = {
+  name: 'Lawrence',
+  age: 27
+};
 
-for (let n in arr) {
-  // what will n be?
-  console.log(n);
+for(let key in Pogi){
+  console.log(`${key} -> ${Pogi[key]}`);
 }
