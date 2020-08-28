3ae899c2c7ce5a39a441f357f5d3b20966e3a2b5
lawrencedovin <lawrence@qwikwire.com>
2020-08-28 18:03:10 -0400

    Added a try catch example

---
+++ try-catch/lib/script.js
@@ -0,0 +1,17 @@
+// function displayInitials(user){
+//   let firstNameLetter = user.firstName[0].toUpperCase();
+//   let lastNameLetter = user.lastName[0].toUpperCase();
+//   return `Hello ${firstNameLetter}.${lastNameLetter}`;
+}
+
+function displayInitials(user){
+  let firstNameLetter;
+  let lastNameLetter;
+  try {
+    firstNameLetter = user.firstName[0].toUpperCase();
+    lastNameLetter = user.lastName[0].toUpperCase();
+  } catch(e){
+    return "Invalid input!";
+  }
+  return `Hello ${firstNameLetter}.${lastNameLetter}`;
+}
