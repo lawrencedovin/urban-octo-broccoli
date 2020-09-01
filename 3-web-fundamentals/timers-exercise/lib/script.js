e1f00ced9e109a01e59b306ec6bb20ad767dec7b
lawrencedovin <lawrence@qwikwire.com>
2020-09-01 15:12:42 -0400

    Completed randomGame challenge

--- 3-web-fundamentals/timers-exercise/lib/script.js
+++ 3-web-fundamentals/timers-exercise/lib/script.js
@@ -1,12 +1,20 @@
 function countDown(seconds) {
   seconds--;
-  let timer = setInterval(() => {
-    if(seconds === 0) {
-      clearInterval(timer);
-      console.log('DONE!');
-    }
+  setInterval(() => {
+    if(seconds === 0) clearInterval(1), console.log('DONE!');
     else console.log(seconds), seconds--;
   }, 1000);
 }
 
 countDown(5);
+
+function randomGame(){
+  let counter = 1;
+  setInterval(() => {
+    let number = Math.random();
+    if(number > .75) clearInterval(1), console.log(`It took ${counter} amount of tries`);
+    counter++;
+  }, 1000);
+}
+
+randomGame();
