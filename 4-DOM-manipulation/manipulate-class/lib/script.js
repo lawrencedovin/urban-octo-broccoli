8ff8d213dfb6682c0f5daf7fd169cd54ea1081d1
lawrencedovin <lawrence@qwikwire.com>
2020-09-01 20:50:38 -0400

    Added happy sad example

--- 4-DOM-manipulation/manipulate-class/lib/script.js
+++ 4-DOM-manipulation/manipulate-class/lib/script.js
@@ -13,4 +13,11 @@ todo.addEventListener('click', () => {
 // todo.classList.add("completed");
 // todo.classList.remove("completed");
 
+const h1 = document.querySelector("h1");
+
+setInterval(() => {
+  h1.classList.contains('big') ? h1.innerText = "SAD" : h1.innerText = "HAPPY";
+  h1.classList.toggle('big');
+  h1.classList.toggle('small');
+}, 1000);
 
