1cd9a214a16de70e47c675f1efeacc682a2d688e
lawrencedovin <lawrence@qwikwire.com>
2020-09-01 20:41:48 -0400

    Added manipulation of classes example

---
+++ 4-DOM-manipulation/manipulate-class/lib/script.js
@@ -0,0 +1,16 @@
+const todo = document.querySelector('li');
+
+// for(item of todo){
+//   item.addEventListener('click', () => {
+//     item.classList.toggle("completed");
+//   });
+// }
+
+todo.addEventListener('click', () => {
+  todo.classList.toggle("completed");
+  // If completed is already on there remove it. Else add it
+});
+// todo.classList.add("completed");
+// todo.classList.remove("completed");
+
+
