c31c3c3497b7485a87072fca17285fb1c831c925
lawrencedovin <lawrence@qwikwire.com>
2020-09-02 20:53:50 -0400

    Ädded added/remove friends example.

--- 4-DOM-manipulation/add-friends-todo/lib/script.js
+++ 4-DOM-manipulation/add-friends-todo/lib/script.js
@@ -1,8 +1,27 @@
-const removeButtons = document.querySelectorAll("li>button");
+const form = document.querySelector("#add-friend");
+const input = document.querySelector("#first-name");
+const friendList = document.querySelector("#friend-list");
 
-for(btn of removeButtons) {
-  btn.addEventListener('click', (e) => {
-    console.log("You clicked remove");
+friendList.addEventListener('click', (e) => {
+  if(e.target.tagName === "BUTTON") {
     e.target.parentElement.remove();
-  });
-}
+  } 
+  else if (e.target.tagName === "LI") {
+    e.target.classList.toggle("best-friend");
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const newFriend = document.createElement('li');
+  const removeBtn = document.createElement('button');
+
+  removeBtn.innerText = 'Unfriend';
+  newFriend.innerText = input.value;
+  
+  newFriend.appendChild(removeBtn);
+  friendList.appendChild(newFriend);
+  
+  input.value = '';
+});
