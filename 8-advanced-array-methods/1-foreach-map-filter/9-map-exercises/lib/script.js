a2cf8982565a95a595e3f218e951bac652ffe5ff
lawrencedovin <lawrence@qwikwire.com>
2020-09-14 17:56:08 -0400

    Added map exercises

--- 8-advanced-array-methods/1-foreach-map-filter/9-map-exercises/lib/script.js
+++ 8-advanced-array-methods/1-foreach-map-filter/9-map-exercises/lib/script.js
@@ -1,81 +1,53 @@
-// forEach //
+// map //
 // doubleValues
-function doubleValues(arr) {
-  const doubleArray = [];
-  arr.forEach((num) => {
-    doubleArray.push(num*2);
+function doubleValuesWithMap(arr) {
+  return arr.map((num) => {
+    return num*2;
   });
-  return doubleArray;
 }
 
-console.log(doubleValues([1,2,3]));
-console.log(doubleValues([5,1,2,3,10]));
+console.log(doubleValuesWithMap([1,2,3])); // [2,4,6]
+console.log(doubleValuesWithMap([1,-2,-3])); // [2,-4,-6]
 
-// onlyEvenValues
-function onlyEvenValues(arr) {
-  const evenArray = [];
-  arr.forEach((num) => {
-    if(num % 2 === 0) evenArray.push(num);
-  });
-  return evenArray;
+// valTimesIndex
+function valTimesIndex(arr){
+  return arr.map((val, i) => {
+    return val*i;
+  }); 
 }
 
-console.log(onlyEvenValues([1,2,3])); // [2]
-console.log(onlyEvenValues([5,1,2,3,10])); // [2,10]
+console.log(valTimesIndex([1,2,3])); // [0,2,6]
+console.log(valTimesIndex([1,-2,-3])); // [0,-2,-6]
 
-// showFirstAndLast
-function showFirstAndLast(arr) {
-  const firstLastArray = [];
-  arr.forEach((letter) => {
-    firstLastArray.push(letter[0]+letter[letter.length-1]);
+// extractKey
+function extractKey(arr, key){
+  return arr.map((value) => {
+    return value[key];
   });
-  return firstLastArray;
 }
 
-console.log(showFirstAndLast(['colt','matt', 'tim', 'test'])); // ["ct", "mt", "tm", "tt"]
-console.log(showFirstAndLast(['hi', 'goodbye', 'smile'])); // ['hi', 'ge', 'se']
-
-// addKeyAndValue
-function addKeyAndValue(arr, key, value) {
-  arr.forEach((val) => {
-    val[key] = value;
-  });
-  return arr;
-}
-console.log(addKeyAndValue(
+console.log(extractKey(
   [
     {name: 'Elie'},
     {name: 'Tim'},
     {name: 'Matt'},
     {name: 'Colt'}
   ],
-    'title',
-    'instructor'
-  ));
-  
-  /*
-    [
-      {name: 'Elie', title:'instructor'},
-      {name: 'Tim', title:'instructor'},
-      {name: 'Matt', title:'instructor'},
-      {name: 'Colt', title:'instructor'}
-    ]
-  */
+  'name'
+));
 
-// vowelCount
-function vowelCount(str) {
-  let splitString = str.split("");
-  let obj = {};
-  let vowels = 'aeiou';
-  splitString.forEach((char) => {
-    let lowerCase = char.toLowerCase();
-    if(vowels.indexOf(lowerCase) !== -1) {
-      obj[lowerCase] ? obj[lowerCase]++ : obj[lowerCase] = 1;
-    }
+// extractFullName
+function extractFullName(arr) {
+  return arr.map((value) => {
+    return `${value.first} ${value.last}`;
+    // return `${value[Object.keys(value)[0]]}  ${value[Object.keys(value)[1]]}`;
   });
-  return obj;
 }
 
-console.log(vowelCount('Elie')); // {e:2,i:1};
-console.log(vowelCount('I Am awesome and so are you')); // {i: 1, a: 4, e: 3, o: 3, u: 1})
+console.log(extractFullName([
+  {first: 'Elie', last:"Schoppik"},
+  {first: 'Tim', last:"Garcia"},
+  {first: 'Matt', last:"Lane"},
+  {first: 'Colt', last:"Steele"}
+]));
 
