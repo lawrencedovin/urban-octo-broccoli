97015583dbac82e69a5b00c1c38bd3b2ab8f77b1
lawrencedovin <lawrence@qwikwire.com>
2020-09-15 14:25:23 -0400

    Writing find and findIndex example

---
+++ 8-advanced-array-methods/2-some-every-find-findIndex/13-writing-find-findindex/lib/script.js
@@ -0,0 +1,28 @@
+// function myFind(arr, callback){
+//   let myArray = [];
+//   for(let i = 0;  i < arr.length; i++){
+//     if(callback(arr[i], i, arr)) myArray.push(arr[i]);
+//   }
+//   return myArray[0];
+// }
+
+function myFind(arr, callback) {
+  for(let i = 0;  i < arr.length; i++){
+    if(callback(arr[i], i, arr)) return arr[i];
+  }
+}
+
+function myFindIndex(arr, callback) {
+  for(let i = 0;  i < arr.length; i++){
+    if(callback(arr[i], i, arr)) return i;
+  }
+}
+
+console.log(myFind([1,2,3,4,5], (num) => {
+  return num % 2 === 0;
+} ));
+
+
+console.log(myFindIndex([1,2,3,4,5], (num) => {
+  return num % 2 === 0;
+} ));
