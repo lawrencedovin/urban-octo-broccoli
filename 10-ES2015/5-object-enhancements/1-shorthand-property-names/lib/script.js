b34db8b65dfd08f3ce63d638272234a47ba72a0c
lawrencedovin <lawrence@qwikwire.com>
2020-09-18 10:01:04 -0400

    Added shorthand property names examples

---
+++ 10-ES2015/5-object-enhancements/1-shorthand-property-names/lib/script.js
@@ -0,0 +1,12 @@
+function person(first, last, age) {
+  return {
+  first,
+  last,
+  age,
+  isAlive: true
+  };
+}
+
+// if the key & value have the share the name
+
+console.log(person('lawrence', 'dovin', 27));
