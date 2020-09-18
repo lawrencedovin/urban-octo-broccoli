0cb1bd010e91358829172c5f3faa59bed60908fa
lawrencedovin <lawrence@qwikwire.com>
2020-09-18 19:52:53 -0400

    Added array destructuring array example.

---
+++ 10-ES2015/6-destructuring/3-array-destructuring/lib/script.js
@@ -0,0 +1,9 @@
+const students = [
+    { name: 'Drake', gpa: 4.6 },
+    { name: 'Henry', gpa: 4.4 },
+    { name: 'Tung', gpa: 4.0 },
+    { name: 'Harry', gpa: 3.8 },
+    { name: 'Ant', gpa: 3.2 }
+];
+
+const [first, ...losers] = students;
