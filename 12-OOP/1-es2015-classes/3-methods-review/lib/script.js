ba55c084bb16de4f0d003e7b09cf1d7d55452092
lawrencedovin <lawrence@qwikwire.com>
2020-09-26 15:32:02 -0400

    Added methods review examples

---
+++ 12-OOP/1-es2015-classes/3-methods-review/lib/script.js
@@ -0,0 +1,21 @@
+// const add = (a, b) => a + b;
+// const mult = (a, b) => a * b;
+// const square = (a) => a ** 2;
+// const power = (a, b) => a ** b;
+
+// const myMath = {};
+// myMath.add = add;
+// myMath.mult = mult;
+
+// const myMath = {add, mult, square, power};
+
+const myMath = {
+  add(a, b) {
+    return a + b;
+  },
+  mult(a, b) {
+    return a * b;
+  }
+};
+
+// console.log(myMath.add(3,4));
