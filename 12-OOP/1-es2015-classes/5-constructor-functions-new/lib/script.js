5bc4440d79961745057b1f09c49c0c581079e69f
lawrencedovin <lawrence@qwikwire.com>
2020-09-27 23:13:02 -0400

    Added constructor functions and new examples

---
+++ 12-OOP/1-es2015-classes/5-constructor-functions-new/lib/script.js
@@ -0,0 +1,25 @@
+const rightTriangle = {
+  a: 9,
+  b: 12,
+  printThis() {
+    console.log(this);
+  },
+  getArea() {
+    return this.a * this.b / 2;
+  },
+  getHypotenuse() {
+    this.printThis();
+    return Math.sqrt(this.a ** 2 + this.b ** 2);
+  }
+};
+
+function Triangle(a, b) {
+  this.a = a;
+  this.b = b;
+  this.getArea = function() {
+    return this.a * this.b / 2;
+  }
+  this.getHypotenuse = function() {
+    return Math.sqrt(this.a ** 2 + this.b ** 2);
+  }
+}
