d37d4fc4c0e631175c98f342f2b124ef5230f6a5
lawrencedovin <lawrence@qwikwire.com>
2020-09-30 18:39:56 -0400

    Added jQuery css classes example.

---
+++ 13-jquery-bootstrap/1-jquery/3-css-classes/lib/script.js
@@ -0,0 +1,17 @@
+$('li').css('color');
+$('li').css('font-size', '20px');
+// Sets CSS styles
+
+const bigTealStyles = {color: 'teal', 'font-size': '40px'}
+$('h1').css(bigTealStyles);
+// Sets multiple properties
+
+
+$('h1').addClass('highlight');
+// Adds a CSS class
+
+$('li').toggleClass('highlight');
+// Added highlight class to all li tags
+
+$('li').toggleClass('highlight');
+// Removed highlight class to all li tags
