384823d8edcfbd4ed9dee4ec78b5234df984c70d
lawrencedovin <lawrence@qwikwire.com>
2020-09-30 20:22:26 -0400

    jQuery events examples

---
+++ 13-jquery-bootstrap/1-jquery/7-events/lib/script.js
@@ -0,0 +1,20 @@
+$('img').click(() => {
+    alert('HOWDY!');
+});
+// Adds click event listener to all imgs
+
+$('li').on('mouseenter', () => {
+     alert('THERE IS A SNAKE IN MY BOOT');
+});
+
+$('li').on('mouseleave', () => {
+    alert('just a prank bro.');
+});
+
+$('img').on('mouseenter', () => {
+    $(this).css('border', '10px solid blue');
+});
+
+$('img').on("click", function() {
+    $(this).remove();
+})
