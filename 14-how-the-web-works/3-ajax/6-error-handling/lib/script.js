a58719b4e4a593576e5e04a713b606395c2ed760
lawrencedovin <lawrence@qwikwire.com>
2020-10-07 20:13:19 -0400

    Added error handling example

---
+++ 14-how-the-web-works/3-ajax/6-error-handling/lib/script.js
@@ -0,0 +1,24 @@
+const randomDogeButton = document.querySelector("#getRandomDoge");
+const dogeByBreedButton = document.querySelector("#getDogeByBreed")
+const dogePic = document.querySelector("img");
+const dogeInput = document.querySelector("#dogeInput");
+
+async function getRandomDoge() {
+  const res = await axios.get('https://dog.ceo/api/breeds/image/random');
+  const {message} = res.data;
+  dogePic.src = message;
+}
+
+async function getDogeByBreed() {
+  try {
+    const url = `https://dog.ceo/api/breed/${dogeInput.value}/images/random`;
+    const res = await axios.get(url);
+    const { message } = res.data;
+    dogePic.src = message;
+  } catch (e) {
+    alert("RUFF RUFF!");
+  }
+}
+
+randomDogeButton.addEventListener("click", getRandomDoge);
+dogeByBreedButton.addEventListener("click", getDogeByBreed);
