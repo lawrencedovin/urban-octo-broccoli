1ead0d283f13a7de8720f3c7b7c28b0936b668d0
lawrencedovin <lawrence@qwikwire.com>
2020-10-09 14:35:20 -0400

    Added post get sample using hack or snooze

--- 14-how-the-web-works/3-ajax/9-hack-or-snooze/lib/script.js
+++ 14-how-the-web-works/3-ajax/9-hack-or-snooze/lib/script.js
@@ -1,33 +1,56 @@
-// async function getUsers() {
-//   const res = await axios.get(
-//     "https://hack-or-snooze-v3.herokuapp.com/users?skip=0&limit=10"
-//   );
-//   console.log(res);
-// }
+async function getUsers(token) {
+  const res = await axios.get("https://hack-or-snooze-v3.herokuapp.com/users", {
+    params: { token },
+  });
+  // takes the token and adds it as a part of the query string
+  // console.log(res);
+}
 
-// async function getUsers() {
-//   const res = await axios.get(
-//     "https://hack-or-snooze-v3.herokuapp.com/signup"
-//   );
-//   console.log(res);
-// }
-
-// getUsers();
-
-// async function signUp(name, username, password) {
-//   const res = await axios.post("https://hack-or-snooze-v3.herokuapp.com/signup", 
-//   {
-//     user: { name, username, password }
-//   });
-//   console.log(res);
-// }
-
-async function signUp(username, password, name) {
-  const res = await axios.post("https://hack-or-snooze-v3.herokuapp.com/signup", { user: {
-    name, username, password }})
+async function signUp(name, username, password) {
+  const res = await axios.post(
+    "https://hack-or-snooze-v3.herokuapp.com/signup",
+    {
+      user: { name, username, password },
+    }
+  );
   console.log(res);
 }
 
+async function login(username, password) {
+  const res = await axios.post(
+    "https://hack-or-snooze-v3.herokuapp.com/login",
+    {
+      user: { username, password },
+    }
+  );
+  console.log(res);
+  // logs user with details
+  return res.data.token;
+  // returns the unique token
+}
 
-// signUp('butters', 'chickenbutter', '12312');
-signUp('butterschicken', '238197sadhj', 'butters the chicken');
+async function getUsersWithAuth() {
+  const token = await login("pokemonmaster1993", "123abc");
+  // Logging in with these credentials and gives back a token
+  getUsers(token);
+  // getUsers returns that user using the unique
+  // token that is returned from the login function
+}
+async function createStory() {
+  const token = await login("pokemonmaster1993", "123abc");
+  const newStory = {
+    token,
+    story: {
+      author: 'Lawrence',
+      title: 'Puberty, the horror',
+      url: 'http://puberty.com'
+    }
+  }
+  const res = await axios.post("https://hack-or-snooze-v3.herokuapp.com/stories",  newStory);
+  console.log(res);
+}
+
+// signUp('Lawrence Dovin', 'pokemonmaster1993', '123abc');
+//login("pokemonmaster1993", "123abc");
+// getUsersWithAuth();
+// createStory();
