63f4a585cb3cf18241aeb0be7535f29e9f2d5676
Lawrence Dovin <lawrencedovin@gmail.com>
2020-10-21 17:01:18 -0400

    Added running logic example

--- 18-python-fundamentals/python-introduction/12-equality.py
+++ 18-python-fundamentals/python-introduction/12-equality.py
@@ -1,3 +1,3 @@
 7 == "7" #False
 [1, 2, 3] == [1, 2, 3] #True, Structures with same items are equal
-[1, 2] is [1, 2] #False, Checks if object Identity is the same
+[1, 2] is [1, 2] #False, Checks if object Identity is the same/ equality reference
---
+++ 18-python-fundamentals/python-introduction/14-bouncer.py
@@ -0,0 +1,6 @@
+age = 19
+guests = 0
+
+if age >= 18:
+    print("You can come in!")
+    guests += 1
