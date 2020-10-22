237bd5a4065c3202aabea46c418a27bb130194a0
Lawrence Dovin <lawrencedovin@gmail.com>
2020-10-22 17:08:55 -0400

    Added string methods examples

---
+++ 18-python-fundamentals/2-python-data-structures/7-string-methods.py
@@ -0,0 +1,27 @@
+msg = 'dskljjasdkljhahk'
+
+msg.count('h')
+
+person = "Mr. Jones"
+
+person.startswith("Mr.")
+
+person.find('.')
+
+'4321123321'.isdigit()
+
+chickens = ['butters', 'lady gray', 'stevie chicks']
+
+"|".join(chickens)
+
+".".join("LOL")
+
+"LOL".lower()
+
+"asddassa".upper()
+
+"mrs kitty cat".capitalize()
+
+things = 'apples-tomatoes-pickles'
+things.replace('-','=')
+things.replace('-','=', 1)
