c3222d3f03d3ad7b9a08026963e0aa15e3ae274d
Lawrence Dovin <lawrencedovin@gmail.com>
2020-10-22 15:32:49 -0400

    Added python slicing examples

---
+++ 18-python-fundamentals/2-python-data-structures/3-slicing.py
@@ -0,0 +1,11 @@
+vegan_no_nos = ['eggs', 'meat', 'milk', 'fish', 'figs']
+
+print(vegan_no_nos[2:4:1])
+print(vegan_no_nos[0:5:1])
+print(vegan_no_nos[0:5:2])
+
+nums = list(range(0,100,1))
+print(nums[50:60:1])
+
+print(nums[::5])
+print(nums[::-10])
