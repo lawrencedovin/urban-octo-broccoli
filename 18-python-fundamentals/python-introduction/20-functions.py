32d0634a0cb62f5a68a1060e604cefc81e7d721d
Lawrence Dovin <lawrencedovin@gmail.com>
2020-10-22 09:57:03 -0400

    Updated python function examples

--- 18-python-fundamentals/python-introduction/20-functions.py
+++ 18-python-fundamentals/python-introduction/20-functions.py
@@ -6,3 +6,16 @@ def divide(a, b):
     if type(a) is int and type(b) is int:
         return a/b
     return 'a and b must be integers!'
+
+
+def send_email(to_email, from_email, subject, body):
+    email = f"""
+        to: {to_email}
+        from: {from_email}
+        subject: {subject}
+        ----------------------
+        body: {body}
+    """
+    print(email)
+
+send_email(subject="Meow", to_email="blu@g.com", from_email="raisin@g.com", body="Hi blue, u r bad cat")
