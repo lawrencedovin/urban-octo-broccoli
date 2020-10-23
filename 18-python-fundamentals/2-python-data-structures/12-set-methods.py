37c16b5dcc8205e323c1f7b98016035e1546db05
Lawrence Dovin <lawrencedovin@gmail.com>
2020-10-23 11:45:20 -0400

    Added set methods examples

---
+++ 18-python-fundamentals/2-python-data-structures/12-set-methods.py
@@ -0,0 +1,12 @@
+languages = {'ruby', 'python', 'python', 'javascript'}
+
+print('scala' in languages)
+
+languages.add('scala')
+print(languages)
+
+languages.pop()
+print(languages)
+
+languages.remove('ruby')
+print(languages)
