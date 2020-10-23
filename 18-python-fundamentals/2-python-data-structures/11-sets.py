da87c1d2004dd3913c294393aa6dc4ddb2761ea1
Lawrence Dovin <lawrencedovin@gmail.com>
2020-10-23 11:35:41 -0400

    Added set examples

---
+++ 18-python-fundamentals/2-python-data-structures/11-sets.py
@@ -0,0 +1,9 @@
+languages = {'ruby', 'python', 'python', 'javascript'}
+print(languages)
+
+voted_languages = ['ruby', 'python', 'python', 'python', 'javascript']
+set_voted_languages = set(voted_languages)
+
+print(set_voted_languages)
+
+things = {2, 'snake', 'mario', True}
