ad9bebac2d829a1cae5a4e378bcdebb0c7e88b17
Lawrence Dovin <lawrencedovin@gmail.com>
2020-10-26 11:41:16 -0400

    Added inheritance example

---
+++ 18-python-fundamentals/4-python-oop/4-inheritance/triangle.py
@@ -0,0 +1,31 @@
+from math import sqrt
+from random import randint
+
+class Triangle: 
+    "Right triangle."
+
+    def __init__(self, a, b):
+        # equivalent to the constructor
+         self.a = a
+         self.b = b
+        # self represents the instance
+    
+    @classmethod
+    def random(cls):
+        return cls(randint(1, 20), randint(1, 20))
+    # alters the behavior of the method that comes after it
+
+    def get_hypotenuse(self):
+        return sqrt(self.a ** 2 + self.b ** 2)
+    
+    def get_area(self):
+        return self.a * self.b / 2
+
+    def describe(self):
+        return f"I am a triangle with side: {self.a} & {self.b}"
+
+    
+
+    
+
+
