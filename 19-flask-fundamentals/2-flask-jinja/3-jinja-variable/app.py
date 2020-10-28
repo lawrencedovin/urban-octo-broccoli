06817eef055d9c329b4487c7129861f46bee13e6
Lawrence Dovin <lawrencedovin@gmail.com>
2020-10-28 09:59:12 -0400

    Added Jinja Variables example

---
+++ 19-flask-fundamentals/2-flask-jinja/3-jinja-variable/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from random import randint
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'oh-so-secret'
+debug = DebugToolbarExtension(app)
+
+@app.route('/hello')
+def say_hello():
+    """Shows hello page"""
+    return render_template("hello.html")
+
+@app.route('/lucky')
+def lucky_number():
+    "Example of simple dynamic template"
+
+    num = randint(1, 100)
+
+    return render_template("lucky.html", lucky_number=num, msg="You sooo lucky")
