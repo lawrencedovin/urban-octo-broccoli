aa27bb8da050da258d766158d59f4b5fac4e0017
Lawrence Dovin <lawrencedovin@gmail.com>
2020-10-31 12:21:29 -0400

    Answer to survey links to next question

--- 19-flask-fundamentals/2-flask-jinja/flask-survey/app.py
+++ 19-flask-fundamentals/2-flask-jinja/flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from random import randint, choice, sample
 from operator import itemgetter
@@ -6,21 +6,39 @@ from surveys import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'oh-so-secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-RESPONSES = []
+responses = []
+SATISFACTION_SURVEY = surveys["satisfaction"]
 # answers to the survey questions stored in responses will look like ['Yes', 'No', 'Less than $10,000', 'Yes']
 
 @app.route('/')
 def get_home():
-    title = surveys["satisfaction"].title
-    instructions = surveys["satisfaction"].instructions
+    title = SATISFACTION_SURVEY.title
+    instructions = SATISFACTION_SURVEY.instructions
 
     return render_template('home.html' , title=title, instructions=instructions)
 
 @app.route('/questions/<int:id>')
 def get_question(id):
-    question = surveys["satisfaction"].questions[id].question
-    return render_template('question.html', question=question)
+    question = SATISFACTION_SURVEY.questions[id].question
+    choices = SATISFACTION_SURVEY.questions[id].choices
+    return render_template('question.html', question=question, choices=choices, id=id)
+
+
+@app.route('/answer', methods=['POST'])
+def get_answer():
+    response = request.form["answer"]
+    responses.append(response)
+    if len(SATISFACTION_SURVEY.questions) > len(responses):
+        return redirect(f'/questions/{len(responses)}')
+    else :
+        return redirect('/complete')
+    
+@app.route('/complete')
+def complete_survey():
+    return render_template('complete.html')
+
 
 
