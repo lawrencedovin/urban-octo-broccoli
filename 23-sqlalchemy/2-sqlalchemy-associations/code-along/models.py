f42b50dc27123c44aa8c6c3f2881f25cc7eef3bc
Lawrence Dovin <lawrencedovin@gmail.com>
2020-11-30 16:44:22 -0500

    Added foreign key to Employees for dept_code

--- 23-sqlalchemy/2-sqlalchemy-associations/code-along/models.py
+++ 23-sqlalchemy/2-sqlalchemy-associations/code-along/models.py
@@ -25,3 +25,4 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Text, nullable=False, unique=True)
     state = db.Column(db.String(2), nullable=False, default='CA')
+    dept_code = db.Column(db.Text, db.ForeignKey('departments.dept_code'))
