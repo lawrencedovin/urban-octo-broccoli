82aa52ced27b2f4c7eec5a7e8110b3b248a739b8
Lawrence Dovin <lawrencedovin@gmail.com>
2020-12-21 12:39:58 -0500

    Added todo json flask example pt 1/2

---
+++ 24-flask-intermediate/3-restful-json-apis/code-along/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from models import *
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///todos_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+
+connect_db(app)
+
+@app.route('/api/todos')
+def list_todos():
+    all_todos = [todo.serialize() for todo in Todo.query.all()]
+    return jsonify(todos=all_todos)
+
+@app.route('/api/todos/<int:id>')
+def get_todo(id):
+    todo = Todo.query.get_or_404(id)
+    return jsonify(todo=todo.serialize())
+
+@app.route('/api/todos', methods=["POST"])
+def create_todo():
+    new_todo = Todo(title=request.json["title"])
+    db.session.add(new_todo)
+    db.session.commit()
+    response_json = jsonify(todo=new_todo.serialize())
+    return (response_json, 201)
