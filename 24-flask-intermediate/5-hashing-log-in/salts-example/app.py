b62783678c13fe97182aed91c8c5cbfe7335fde3
Lawrence Dovin <lawrencedovin@gmail.com>
2020-12-25 21:56:33 -0500

    Added check password for hash table

--- 24-flask-intermediate/5-hashing-log-in/salts-example/app.py
+++ 24-flask-intermediate/5-hashing-log-in/salts-example/app.py
@@ -3,3 +3,9 @@ with open('xato-net-10-million-passwords-100000.txt', 'r') as f:
     for line in f:
         pwd = line.strip()
         hash_table[hash(pwd)] = pwd
+
+def checkPassword(hash_table, key):
+    if key in hash_table.keys():
+        print(f'Value = {hash_table[key]}')
+    else:
+        print('No key found')
