5c85f676852b5aef7deb51d21cdab028aa0c1db2
Lawrence Dovin <lawrencedovin@gmail.com>
2021-04-26 20:29:42 -0400

    Added promise.race example

--- 31-node/1-javascript-promises/promise-all-and-race/app.js
+++ 31-node/1-javascript-promises/promise-all-and-race/app.js
@@ -1,3 +1,21 @@
+// let fourPokemonPromises = [];
+
+// for (let i = 1; i < 5; i++) {
+//     fourPokemonPromises.push(
+//         axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+//     );
+// }
+
+// Promise.all(fourPokemonPromises)
+//     .then(pokemonArr => {
+//         for (pokemon of pokemonArr) {
+//             console.log(pokemon.data.name);
+//         }
+//         // pokemonArr.forEach(p => console.log(p.data.name))
+//         // console.log(pokemonArr)
+//     })
+//     .catch(err => console.log(err));
+
 let fourPokemonPromises = [];
 
 for (let i = 1; i < 5; i++) {
@@ -6,12 +24,8 @@ for (let i = 1; i < 5; i++) {
     );
 }
 
-Promise.all(fourPokemonPromises)
-    .then(pokemonArr => (
-        for (let response of pokemonArr) {
-            console.log(response.data.name);
-        }
-        // pokemonArr.forEach(p => console.log(p.data.name))
-        // console.log(pokemonArr)
-    ))
-    .catch(err => console.log(err));
+Promise.race(fourPokemonRace)
+    .then(res => {
+        console.log(`${res.data.name} won the race!`)
+    })
+    .catch(err => console.log(err))
