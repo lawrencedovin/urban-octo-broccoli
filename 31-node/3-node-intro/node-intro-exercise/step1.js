e4ae6b8fe70b1aaf608d8b133d80c287acdae61b
Lawrence Dovin <lawrencedovin@gmail.com>
2021-04-29 15:11:50 -0400

    Added step 1 solution

--- 31-node/3-node-intro/node-intro-exercise/step1.js
+++ 31-node/3-node-intro/node-intro-exercise/step1.js
@@ -1,11 +1,15 @@
 const argv = process.argv;
 const fs = require('fs');
 const file_location = argv[argv.length -1 ];
-// for (let i = 0; i < argv.length; i += 1) {
-//     console.log(i, argv[i]);
-// }
-console.log(file_location);
 
 function cat(path) {
-
+    fs.readFile(path, 'utf8', (err, data) => {
+        if(err) {
+            console.log(err);
+            process.exit(1);
+        }
+        console.log(`${data}`);
+    })
 }
+
+cat(file_location);
