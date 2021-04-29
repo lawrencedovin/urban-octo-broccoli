58ac452d3163e01edf5bb2f04cd833d45f3090d0
Lawrence Dovin <lawrencedovin@gmail.com>
2021-04-29 15:08:09 -0400

    Added file location to argv

---
+++ 31-node/3-node-intro/node-intro-exercise/step1.js
@@ -0,0 +1,11 @@
+const argv = process.argv;
+const fs = require('fs');
+const file_location = argv[argv.length -1 ];
+// for (let i = 0; i < argv.length; i += 1) {
+//     console.log(i, argv[i]);
+// }
+console.log(file_location);
+
+function cat(path) {
+
+}
