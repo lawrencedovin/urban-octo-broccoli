a7083195e092a8b7dfb93e3e44c5dfb6f565c168
Lawrence Dovin <lawrencedovin@gmail.com>
2021-05-19 17:18:12 -0400

    Added update router

--- 35-express-postgresql/1-node-pg-intro/6-creating/routes/users.js
+++ 35-express-postgresql/1-node-pg-intro/6-creating/routes/users.js
@@ -35,10 +35,28 @@ router.post('/', async (req, res, next) => {
              VALUES ($1, $2)
              RETURNING id, name, type`, 
              [name, type]);
-        return res.status(201).json(results.rows);
+        return res.status(201).json(results.rows[0]);
     }
     catch(e) {
         return next(e);
     }
 });
+
+router.patch('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name, type } = req.body;
+        const results = await db.query(
+            `UPDATE users SET name=$1, type=$2
+            WHERE id = $3
+            RETURNING id, name, type`,
+            [name, type, id]
+        );
+        return res.json(results.rows[0]);
+    }
+    catch(e) {
+        return next(e);
+    }
+});
+
 module.exports = router;
