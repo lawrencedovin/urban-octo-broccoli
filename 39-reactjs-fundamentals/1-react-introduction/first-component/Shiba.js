8b09e344960e6582f57c6174ee0cae5f08312ede
Lawrence Dovin <lawrencedovin@gmail.com>
2021-06-25 22:24:36 -0400

    Added initial code for first component

---
+++ 39-reactjs-fundamentals/1-react-introduction/first-component/Shiba.js
@@ -0,0 +1,5 @@
+const Shiba = () => {
+    return <img src="https://64.media.tumblr.com/5c8b54765481330a0be4ac7bcde192d5/tumblr_po3xo4muE41wjwprio1_1280.jpg" />
+}
+
+ReactDOM.render(<Shiba/>, document.querySelector('#root'))
