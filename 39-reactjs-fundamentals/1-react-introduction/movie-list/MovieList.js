f3b46d5329f63bbedaae5853c554128cafc34502
Lawrence Dovin <lawrencedovin@gmail.com>
2021-06-25 22:02:54 -0400

    Updated movie list

---
+++ 39-reactjs-fundamentals/1-react-introduction/movie-list/MovieList.js
@@ -0,0 +1,32 @@
+let movies = ['Amadeus', 'Alien', 'Arrival'];
+
+const MovieList = () => {
+	return (
+		<ul>
+			{movies.map(movie => <li>{movie}</li>)}
+		</ul>
+	)
+};
+
+
+// const MovieList = () => {
+// 	return (
+// 		<ul>
+// 			<li>Amadeus</li>
+// 			<li>Alien</li>
+// 			<li>Arrival</li>
+// 		</ul>
+// 	);
+// };
+
+const App = () => {
+	return (
+		<div>
+			<h1>App Component!</h1>
+			<MovieList />
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
