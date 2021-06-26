3f2f643151353e318dd91d4ed75e8d66ca1084b5
Lawrence Dovin <lawrencedovin@gmail.com>
2021-06-26 16:29:14 -0400

    Updated shiba

--- 39-reactjs-fundamentals/1-react-introduction/first-component/Shiba.js
+++ 39-reactjs-fundamentals/1-react-introduction/first-component/Shiba.js
@@ -2,4 +2,13 @@ const Shiba = () => {
     return <img src="https://64.media.tumblr.com/5c8b54765481330a0be4ac7bcde192d5/tumblr_po3xo4muE41wjwprio1_1280.jpg" />
 }
 
-ReactDOM.render(<Shiba/>, document.querySelector('#root'))
+const App = () => {
+	return (
+		<div>
+			<h1>Shiba!</h1>
+			<Shiba />
+		</div>
+	)
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
