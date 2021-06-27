53cff324d0a4453f2620694fcd05021499cda91e
Lawrence Dovin <lawrencedovin@gmail.com>
2021-06-27 16:19:48 -0400

    Added class components example

---
+++ 39-reactjs-fundamentals/1-react-introduction/class-components/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+	return (
+		<div>
+			<h1>Shiba!</h1>
+			<Shiba />
+			<SharPei />
+		</div>
+	)
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
---
+++ 39-reactjs-fundamentals/1-react-introduction/class-components/Shiba.js
@@ -0,0 +1,9 @@
+// const Shiba = () => {
+//     return <img src="https://64.media.tumblr.com/334a651a151edabda5c78979a39b7353/tumblr_osl4c1fHZv1vjwef9o5_500.jpg" />
+// }
+
+class Shiba extends React.Component {
+    render() {
+        return <img src="https://64.media.tumblr.com/334a651a151edabda5c78979a39b7353/tumblr_osl4c1fHZv1vjwef9o5_500.jpg" />;
+    }
+}
