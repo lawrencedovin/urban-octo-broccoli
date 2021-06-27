98a03bdd2d1e7e130d6235a38e0719ec2bb91b94
Lawrence Dovin <lawrencedovin@gmail.com>
2021-06-27 19:54:06 -0400

    Added jsx conditionals example

---
+++ 39-reactjs-fundamentals/2-props-in-react/jsx-conditionals/App.js
@@ -0,0 +1,16 @@
+const App = () => {
+	return (
+		<div>
+			<Animal emoji="🐧" name="Jerry" species="Penguin" />
+			<Animal emoji="🐱‍👤" name="Pringles" species="Cat" />
+			<Animal emoji="🐱" species="Cat" isCute />
+			<Animal />
+			<RandomChoice choices = {['green', 'blue', 'red']}/>
+			<Bouncer age={17} />
+			<Bouncer age={19} />
+			<Bouncer age={21} />
+		</div>
+	)
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
---
+++ 39-reactjs-fundamentals/2-props-in-react/jsx-conditionals/RandomChoice.js
@@ -0,0 +1,7 @@
+const RandomChoice = (props) => {
+    const idx = Math.floor(Math.random() * props.choices.length);
+    const choice = props.choices[idx];
+    return (
+        <h4>Random Choice is: {choice}</h4>
+    );
+}
