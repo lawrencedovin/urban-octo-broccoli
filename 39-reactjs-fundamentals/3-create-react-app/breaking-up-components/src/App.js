d9aec261fc35f505d9624c6937341a98e85a3c58
Lawrence Dovin <lawrencedovin@gmail.com>
2021-06-29 19:56:42 -0400

    Added cra images and assets

--- 39-reactjs-fundamentals/3-create-react-app/breaking-up-components/src/App.js
+++ 39-reactjs-fundamentals/3-create-react-app/breaking-up-components/src/App.js
@@ -1,10 +1,12 @@
 import items from './Items';
 import ShoppingCart from './ShoppingCart';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div>
+      <img src={logo} alt="logo"/>
       <ShoppingCart items={items} username="lawrence" />
       <ShoppingCart items={items} username="anonymous" />
     </div>
