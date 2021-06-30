8e981046f4029c9a8b58bf19fdbc29c79b4752a2
Lawrence Dovin <lawrencedovin@gmail.com>
2021-06-30 16:41:51 -0400

    Added dynamic style attribute

--- 39-reactjs-fundamentals/3-create-react-app/breaking-up-components/src/App.js
+++ 39-reactjs-fundamentals/3-create-react-app/breaking-up-components/src/App.js
@@ -1,11 +1,25 @@
 import items from './Items';
 import ShoppingCart from './ShoppingCart';
+import Alert from './Alert';
 import './App.css';
 import logo from './logo.svg';
 
 function App() {
   return (
     <div>
+
+      <Alert variant="success">
+        <h1>Welcome Back!</h1>
+      </Alert>
+      
+      <Alert variant="danger">
+        <h1>Stranger Danger! uwu</h1>
+      </Alert>
+
+      <Alert variant="warning">
+        <h1>Warning warning. You have been warned.</h1>
+      </Alert>
+
       <img src={logo} alt="logo"/>
       <ShoppingCart items={items} username="lawrence" />
       <ShoppingCart items={items} username="anonymous" />
