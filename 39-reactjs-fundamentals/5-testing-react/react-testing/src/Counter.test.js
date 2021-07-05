c7cedad7ea66b27df1e55dd6cd7ddc0e86c5d141
Lawrence Dovin <lawrencedovin@gmail.com>
2021-07-05 16:30:58 -0400

    Added firing events testing

--- 39-reactjs-fundamentals/5-testing-react/react-testing/src/Counter.test.js
+++ 39-reactjs-fundamentals/5-testing-react/react-testing/src/Counter.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Counter from './Counter';
 
 test('it render w/o crashing', () => {
   render(<Counter />);
-})
+});
 
 test('playing with queries', () => {
     const { getByText } = render(<Counter />);
     getByText(`Let's count`, {exact: false});
-})
+});
+
+test('button clicks', () => {
+    const { getByText} = render(<Counter/>);
+    const h2 = getByText('Current count: 0');
+    const btn = getByText('Add');
+    fireEvent.click(btn);
+    expect(h2).toHaveTextContent('1');
+    expect(h2).not.toHaveTextContent('0');
+});
--- 39-reactjs-fundamentals/5-testing-react/react-testing/src/Toggler.test.js
+++ 39-reactjs-fundamentals/5-testing-react/react-testing/src/Toggler.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Toggler from './Toggler';
 
-it('should start showing text onload', () => {
+// it('should start showing text onload', () => {
+//     const { getByText } = render(<Toggler/>);
+//     const heading = getByText('Hello World');
+//     expect(heading).toHaveClass('Toggler-text');
+//     expect(heading).toBeInTheDocument();
+// });
+
+it('should toggle', () => {
     const { getByText } = render(<Toggler/>);
     const heading = getByText('Hello World');
     expect(heading).toHaveClass('Toggler-text');
+    const btn = getByText('Toggle');
+    
     expect(heading).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(heading).not.toBeInTheDocument();
+
 });
