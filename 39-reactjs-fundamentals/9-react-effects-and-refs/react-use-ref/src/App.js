8d7966d99d8ff0d4e180fa71282401c032600270
Lawrence Dovin <lawrencedovin@gmail.com>
2021-07-12 20:34:02 -0400

    Added use ref timer example

--- 39-reactjs-fundamentals/9-react-effects-and-refs/react-use-ref/src/App.js
+++ 39-reactjs-fundamentals/9-react-effects-and-refs/react-use-ref/src/App.js
@@ -1,12 +1,14 @@
 import Video from './Video';
 import Focuser from './Focuser';
+import Timer from './Timer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       {/* <Video /> */}
-      <Focuser />
+      {/* <Focuser /> */}
+      <Timer />
     </div>
   );
 }
---
+++ 39-reactjs-fundamentals/9-react-effects-and-refs/react-use-ref/src/Timer.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Timer = () => {
+    const [seconds, setSeconds] = useState(0);
+    const timerId = useRef();
+
+    useEffect(() => {
+        timerId.current = setInterval(() => {
+            setSeconds(seconds => seconds + 1)
+        }, 1000);
+
+        return () => {
+            clearInterval(timerId.current);
+        }
+    }, [])
+
+    const stopTimer = () => {
+        clearInterval(timerId.current);
+    }
+
+    return (
+        <>
+            <h1>{seconds}</h1>
+            <button onClick={stopTimer}>Stop</button>
+        </>
+    );
+}
+
+export default Timer;
