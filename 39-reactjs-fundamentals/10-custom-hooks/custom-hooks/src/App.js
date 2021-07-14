d0b2b5664b777d866ddc2e29234a452852869ea5
Lawrence Dovin <lawrencedovin@gmail.com>
2021-07-14 19:44:12 -0400

    Added localstorage custom hook

--- 39-reactjs-fundamentals/10-custom-hooks/custom-hooks/src/App.js
+++ 39-reactjs-fundamentals/10-custom-hooks/custom-hooks/src/App.js
@@ -1,11 +1,15 @@
 import MoodClicker from './MoodClicker';
+import Counter from './Counter';
+import ColorPicker from './ColorPicker';
 import './App.css';
 import './MoodClicker.css';
 
 function App() {
   return (
     <div className="App">
-      <MoodClicker />
+      {/* <MoodClicker /> */}
+      {/* <Counter /> */}
+      <ColorPicker />
     </div>
   );
 }
---
+++ 39-reactjs-fundamentals/10-custom-hooks/custom-hooks/src/Counter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import useLocalStorageState from './hooks/useLocalStorageState';
+
+const Counter = () => {
+
+    const [count, setCount] = useLocalStorageState('count', 0);
+
+    const addToCount = () => {
+        setCount(count => count + 1);
+    }
+
+    return (
+        <>
+            <h4>{count}</h4>
+            <button onClick={addToCount}>Add to Count</button>
+        </>
+    );
+}
+
+export default Counter;
