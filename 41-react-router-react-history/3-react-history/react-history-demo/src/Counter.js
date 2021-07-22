2c214b3371954437956d11932bb396efeb250dac
Lawrence Dovin <lawrencedovin@gmail.com>
2021-07-21 20:28:32 -0400

    Added state in class components example

--- 41-react-router-react-history/3-react-history/react-history-demo/src/Counter.js
+++ 41-react-router-react-history/3-react-history/react-history-demo/src/Counter.js
@@ -3,13 +3,17 @@ import React from 'react';
 class Counter extends React.Component {
     constructor(props) {
         super(props);
-        console.log(this.props);
+        this.state = { count: 0, isHiding: false }
     }
     render() {
         const {color} = this.props;
+        const {count} = this.state;
         return (
             <div>
                 <h1 style={{color}}>I AM COUNTER</h1>
+                <h3>Count is: {count}</h3>
+                <button onClick={() => this.setState({count: count + 1})}>+</button>
+                <button onClick={() => this.setState({count: count - 1})}>-</button>
             </div>
         )
     }
