6cd06a68da0e45bd974a66e31865f26a87cb6da5
Lawrence Dovin <lawrencedovin@gmail.com>
2021-08-15 14:48:21 -0400

    Added sorted frequency solution

--- 47-datastructures-&-algorithms/3 Divide and Conquer/prob-divide-conquer/sorted-frequency.js
+++ 47-datastructures-&-algorithms/3 Divide and Conquer/prob-divide-conquer/sorted-frequency.js
@@ -20,22 +20,22 @@ function findLastIndex(array, value) {
     while(leftIndex <= rightIndex) {
         let middleIndex = Math.ceil((leftIndex + rightIndex) / 2);
 
-        if(array[leftIndex] === value) return leftIndex;
-        if(array[rightIndex - 1] === value) rightIndex = middleIndex - 1;
-        else if(array[leftIndex + 1] !== value) leftIndex = middleIndex + 1;
-        else return middleIndex;
+        if(array[middleIndex] === value 
+            && (array[middleIndex + 1] > value 
+            || array[middleIndex - 1] < value)) {
+                return middleIndex;
+        }
+        else if(array[leftIndex] !== value && array[leftIndex] < value) leftIndex = middleIndex + 1;
+        else if(array[rightIndex] !== value && array[rightIndex] > value) rightIndex = middleIndex - 1;
     }
     return -1;
 }
 
-
-// if(array[rightIndex] === value && array[rightIndex + 1] !== value){
-        //     lastValueFoundIndex = rightIndex;
-        // }
-
 function sortedFrequency(array, value) {
     let firstValueIndex = findFirstIndex(array, value);
-    return firstValueIndex;
+    let lastValueIndex = findLastIndex(array, value);
+    let count = firstValueIndex === -1 ? -1 : (lastValueIndex - firstValueIndex) + 1;
+    return count;
 }
 
 console.log(sortedFrequency([1,1,2,2,2,2,3],2)) // 4
