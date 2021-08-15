e636cdfa3ce5fa12fec8048e7adbb3cb9619fea2
Lawrence Dovin <lawrencedovin@gmail.com>
2021-08-15 10:20:24 -0400

    Added finding first sorted frequency

--- 47-datastructures-&-algorithms/3 Divide and Conquer/prob-divide-conquer/sorted-frequency.js
+++ 47-datastructures-&-algorithms/3 Divide and Conquer/prob-divide-conquer/sorted-frequency.js
@@ -1,5 +1,46 @@
-function sortedFrequency() {
+function findFirstIndex(array, value) {
+    let leftIndex = 0;
+    let rightIndex = array.length - 1;
 
+    while(leftIndex <= rightIndex) {
+        let middleIndex = Math.ceil((leftIndex + rightIndex) / 2);
+
+        if(array[leftIndex] === value) return leftIndex;
+        if(array[rightIndex - 1] === value) rightIndex = middleIndex - 1;
+        else if(array[leftIndex + 1] !== value) leftIndex = middleIndex + 1;
+        else return middleIndex;
+    }
+    return -1;
 }
+
+function findLastIndex(array, value) {
+    let leftIndex = 0;
+    let rightIndex = array.length - 1;
+
+    while(leftIndex <= rightIndex) {
+        let middleIndex = Math.ceil((leftIndex + rightIndex) / 2);
+
+        if(array[leftIndex] === value) return leftIndex;
+        if(array[rightIndex - 1] === value) rightIndex = middleIndex - 1;
+        else if(array[leftIndex + 1] !== value) leftIndex = middleIndex + 1;
+        else return middleIndex;
+    }
+    return -1;
+}
+
+
+// if(array[rightIndex] === value && array[rightIndex + 1] !== value){
+        //     lastValueFoundIndex = rightIndex;
+        // }
+
+function sortedFrequency(array, value) {
+    let firstValueIndex = findFirstIndex(array, value);
+    return firstValueIndex;
+}
+
+console.log(sortedFrequency([1,1,2,2,2,2,3],2)) // 4
+console.log(sortedFrequency([1,1,2,2,2,2,3],3)) // 1
+console.log(sortedFrequency([1,1,2,2,2,2,3],1)) // 2
+console.log(sortedFrequency([1,1,2,2,2,2,3],4)) // -1
 
 module.exports = sortedFrequency
