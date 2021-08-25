fc5e2c7683440d3a73059ba60ca80f4f24195e66
Lawrence Dovin <lawrencedovin@gmail.com>
2021-08-25 08:13:46 -0400

    Added alternative for adding numbers

--- 47-datastructures-&-algorithms/6 Recursion/sum-array-alternative.js
+++ 47-datastructures-&-algorithms/6 Recursion/sum-array-alternative.js
@@ -1,15 +1,9 @@
-const sumArray = (nums) => {
-    //Base Case
+const sum = (nums, i=0) => {
     if(nums.length === 0) return 0;
-
-    //Normal Case
     // nums = [1,2,3]
     //1. 1 + sum([2,3])
     //2. 1 + 2 + sum([3])
     //3. 1 + 2 + 3 + sum([])
     //4. 1 + 2 + 3 + 0
-
-    return nums[0] + sumArray(nums.slice(1));
+    return nums[i] + sum(nums, i+1);
 }
-
-console.log(sumArray([1,2,3]));
