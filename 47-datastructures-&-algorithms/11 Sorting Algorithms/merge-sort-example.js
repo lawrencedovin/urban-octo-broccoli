23d9339b52988bc3db401a08c8c7558e8f71b310
Lawrence Dovin <lawrencedovin@gmail.com>
2021-08-30 07:36:43 -0400

    Added merge function for merge sort

---
+++ 47-datastructures-&-algorithms/11 Sorting Algorithms/merge-sort-example.js
@@ -0,0 +1,28 @@
+const merge = (arr1, arr2) => {
+   const mergedArr = [];
+   let i = 0;
+   let j = 0;
+   // If 1 of these conditions are false then exits
+   while(i < arr1.length && j < arr2.length) {
+      if(arr1[i] < arr2[j]) {
+         mergedArr.push(arr1[i]);
+         i++;
+      }
+      else {
+         mergedArr.push(arr2[j]);
+         j++;
+      }
+   }
+   // Continues to push if 1 of the arrays is already fully merged
+   while(i < arr1.length) {
+      results.push(arr1[i]);
+      i++;
+   }
+   while(j < arr1.length) {
+      results.push(arr2[j]);
+      j++;
+   }
+}
+
+// 1 || 0 - 1+0 = true
+// 1 && 0 - 1*0 = false
