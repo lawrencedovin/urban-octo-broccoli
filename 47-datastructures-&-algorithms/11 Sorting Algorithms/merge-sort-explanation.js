ad9d9b274658b4e45f4c297947c6eea69c02899a
Lawrence Dovin <lawrencedovin@gmail.com>
2021-08-30 07:15:45 -0400

    Added initial explanation of merge sort

---
+++ 47-datastructures-&-algorithms/11 Sorting Algorithms/merge-sort-explanation.js
@@ -0,0 +1,23 @@
+ i        j
+[5,9,20] [1,3,6]
+[1]
+// arr1[i]>arr2[j] ? shift and add arr2[j] to mergedArr and j += 1 : add arr1[i] to mergedArr
+// if all of arr j.length === empty / is done then focus only on incrementing arr i
+
+i           j
+[5,9,20] [1,3,6]
+[1, 3]
+
+i             j
+[5,9,20] [1,3,6]
+[1, 3, 5]
+
+   i          j
+[5,9,20] [1,3,6]
+[1, 3, 5, 6]
+
+     i        j
+[5,9,20] [1,3,6]
+[1, 3, 5, 6, 9, 20]
+
+// end when both arr1.length and arr2.length is 0.
