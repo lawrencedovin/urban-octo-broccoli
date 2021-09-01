ec5666016d077c35ce7164f4015b3d23b7bad6f8
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-01 07:25:40 -0400

    Added problem solving notes

---
+++ 48-interview-prep/1 Problem Solving Patterns/problemSolving.js
@@ -0,0 +1,17 @@
+// Write a function which takes two numbers and returns their sum.
+
+// 1. Can I restate the problem in my own words?
+// A: A function that returns the sum bys adding two numbers.
+
+// 2. What are the inputs that go into the problem?
+// A: Data Type and length.
+
+// 3. What are the outputs that should come from the solution to the problem?
+// A: Just a single number.
+
+// 4. Can the outputs be determined from the inputs?
+// A: Yes, if we have 2 numbers. But for edge cases (0s, strings) etc.
+// we need to know how to error handle.
+
+// 5. How should I label the important pieces of data that are a part of the problem?
+// A: num1, num2, sum
