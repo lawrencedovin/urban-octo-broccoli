a23e72eb1fbd6982732f73d89a97defabb818922
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-01 07:43:55 -0400

    Added breaking it down notes

---
+++ 48-interview-prep/1 Problem Solving Patterns/3-breakingItDown.js
@@ -0,0 +1,40 @@
+// Write a function which takes in a string and returns counts of each
+// character in the string.
+
+// charCount("aaaa");
+// {a: 4}
+
+// charCount("hello");
+// {h: 1, e: 1, l: 2, o: 1}
+
+// charCount("You PIN number is 1234!");
+/*
+    {
+        1: 1,
+        2: 1,
+        3: 1,
+        4: 1,
+        b: 1,
+        e: 1,
+        i: 2,
+        m: 1,
+        n: 2,
+        o: 1,
+        p: 1,
+        r: 2,
+        s: 1,
+        u: 2,
+        y: 1
+    }
+*/
+
+/*
+    function charCount(str) {
+        // create empty object to hold char counts
+        // Loop over each char in str
+            // If char is letter or number - lowercase first.
+                // if char is in object, add 1 to count.
+                // otherwise, add to object and set count to 1.
+        // return object with character counts.
+    }
+*/
