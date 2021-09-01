581168e7074ba7c0e693f2cfa985b1956531c685
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-01 07:32:55 -0400

    Added concrete examples notes

---
+++ 48-interview-prep/1 Problem Solving Patterns/2-concrete-examples.js
@@ -0,0 +1,13 @@
+// Write a function which takes in a string and returns counts of each
+// character in the string.
+
+// 1. Start with simple examples.
+// A: charCount('hello') => {'h': 1, 'e': 1, 'l': 2, 'o': 1}
+
+// 2. Progress to more complex examples.
+// A: charCount('My phone number is (344)-222-3333'). 
+// Are numbers, dashes, spaces included?
+
+// 3. Explore examples with empty inputs.
+
+// 4. Explore examples with invalid inputs.
