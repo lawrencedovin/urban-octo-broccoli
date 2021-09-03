59152c3f425e73c1a114dab091185352a20804b0
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-03 10:17:24 -0400

    added some error handling

--- javascript-tips/average-even-numbers.js
+++ javascript-tips/average-even-numbers.js
@@ -7,14 +7,15 @@ const getAverageOfEvenNumbers = (array) => {
     // sum even numbers
     // count how many even numbers
     // average = sum / count
-
+    if(array.some(isNaN)) return `Input is not a Number Array`;
     const sum = array.filter(number => number % 2 === 0)
-                   .reduce((accumalator, currentNumber) => accumalator + currentNumber);
+                    .reduce((accumalator, currentNumber) => accumalator + currentNumber);
     const count = array.filter(number => number % 2 === 0)
-                     .reduce((accumalator) => accumalator + 1, 0);
-    const average = sum / count;
+                        .reduce((accumalator) => accumalator + 1, 0);
+    const average = sum / count || false;
     return average;
 }
 
 console.log(getAverageOfEvenNumbers([1, 2, 4, 5]));
 console.log(getAverageOfEvenNumbers([1, 2, 4, 8]));
+console.log(getAverageOfEvenNumbers(['1', 12, true, 'fil']));
