429a91da1c182a6a5ce19d11416142d2b71b6727
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-04 14:14:42 -0400

    Added js operators notes

---
+++ 48-interview-prep/3 JS Tricky Parts/must-knows.js
@@ -0,0 +1,19 @@
+// Array / String / Object / Number methods
+
+// type checking / conversion
+
+// JS operators (== vs ===) etc.
+/** 
+ * '==' is for comparing value but not data type, 
+ * '===' is for strictly checking both value and type.
+ * 4 == '4' -> true 
+ * 4 === '4' -> false
+ * */ 
+
+// Asynchronous Code
+
+// AJAX
+
+// XHR / Fetch
+
+// Callbacks / Promises / Async Await
