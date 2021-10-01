417eb973944fc49306dae682d0375476a68565cb
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-01 16:02:30 -0400

    Added Linked List example

--- js-data-structure-examples/linked-list.js
+++ js-data-structure-examples/linked-list.js
@@ -5,3 +5,56 @@
 // Inserting / Removing the tail: O(n) to access + O(1)
 // Inserting / Remove the middle node: O(n) to access + O(1)
 // Searching for a value: O(n)
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
+    traverse() {
+        let currentNode = this.head;
+        while(currentNode) {
+            console.log(currentNode.val);
+            currentNode = currentNode.next;
+        }
+    }
+    search(val) {
+        let currentNode = this.head;
+        while(currentNode) {
+            if(currentNode.val === val) return true;
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+    append(val) {
+        const newNode = new Node(val); 
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        // Current Tail Node sets its next to New Node
+        this.tail.next = newNode;
+        // New Tail is set to New Node
+        this.tail = newNode;
+    }
+}
+
+class Node {
+    constructor(val, next=null) {
+        this.val = val
+        this.next = next
+    }
+}
+
+// antNode -> beeNode -> caterpillarNode
+let caterpillarNode = new Node("caterpillar");
+let beeNode = new Node("bee", caterpillarNode);
+let antNode = new Node("ant", beeNode);
+
+// antNode is at the Head
+let insects = new LinkedList();
+insects.head = antNode;
+insects.tail = caterpillarNode;
+
+insects.append("dragonfly");
+insects.traverse();
