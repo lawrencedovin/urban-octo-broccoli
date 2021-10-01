fc397d7e7e257fd75c223d6787084ed7202771b6
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-01 15:47:15 -0400

    Added initial notes on linked list

---
+++ js-data-structure-examples/linked-list.js
@@ -0,0 +1,7 @@
+// Accessing the head: O(1)
+// Accessing the tail: O(n)
+// Accessing a middle node: O(n)
+// Inserting / Removing the head: O(1)
+// Inserting / Removing the tail: O(n) to access + O(1)
+// Inserting / Remove the middle node: O(n) to access + O(1)
+// Searching for a value: O(n)
