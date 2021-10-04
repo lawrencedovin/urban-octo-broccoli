a467273371fa0f6f902b285683cd6ec5e2e9688f
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-04 16:09:16 -0400

    Updated example for Stack

--- js-data-structure-examples/stack.js
+++ js-data-structure-examples/stack.js
@@ -1,74 +1,88 @@
-// (Last In First Out - Last one in from the front of the list 
+// (Last In First Out - Last in in from the front of the list 
 // is the first one to get out)
 
-class Stack {
-    constructor() {
-        this.first = null;
-        this.last = null;
-        this.size = 0;
-    }
-
-    traverse() {
-        let currentNode = this.first;
-        while(currentNode) {
-            console.log(currentNode.val);
-            currentNode = currentNode.next;
-        }
-    }
-
-    push(val) {
-        const newNode = new Node(val);
-        if(!this.first) {
-            this.first = newNode;
-            this.last = newNode;
-        }
-        // Current Last is pointed to new node
-        this.last.next = newNode;
-        // Last is updated to new node
-        this.last = newNode;
-        // Update size
-        this.size++;
-    }
-
-    pop() {
-        // traverse and if this.next === this.tail then store that value 
-        // update that to the new tail. make that value also point next to null.
-        let currentNode = this.first;
-        let newTail;
-        while(currentNode) {
-            if(currentNode.next === this.last) newTail = currentNode;
-            currentNode = currentNode.next;
-        }
-        console.log(`${this.last.val} has been removed.`);
-        this.last = newTail;
-        this.last.next = null;
-    }
-}
+/** Node: node for a stack. */
 
 class Node {
-    constructor(val, next=null) {
-        this.val = val;
-        this.next = next;
+    constructor(val) {
+      this.val = val;
+      this.next = null;
     }
-}
-
-// antNode -> beeNode -> caterpillarNode
-let caterpillarNode = new Node("caterpillar");
-let beeNode = new Node("bee", caterpillarNode);
-let antNode = new Node("ant", beeNode);
-
-console.log(beeNode);
-
-// antNode is at the Head
-let insects = new Stack();
-insects.first = antNode;
-insects.last = caterpillarNode;
-
-insects.push("dragonfly");
-insects.push("beetle");
-insects.traverse();
-
-console.log("================after pop====================");
-insects.pop("beetle");
-insects.pop("ant");
-insects.traverse();
+  }
+  
+  /** Stack: chained-together nodes where you can
+   *  remove from the top or add to the top. */
+  
+  class Stack {
+    constructor() {
+      this.first = null;
+      this.last = null;
+      this.size = 0;
+    }
+  
+    /** push(val): add new value to end of the stack. Returns undefined. */
+  
+    push(val) {
+      let newNode = new Node(val);
+      if(!this.first) {
+        this.first = newNode;
+        this.last = newNode;
+      }
+      // Add 1 to length
+      this.size += 1;
+      // Point current tail to new tail/ new node
+      this.last.next = newNode;
+      // Update tail and set it to newNode
+      this.last = newNode;
+    }
+  
+    /** pop(): remove the node from the top of the stack
+     * and return its value. Should throw an error if the stack is empty. */
+  
+    pop() {
+      if(this.isEmpty()) throw 'Stack is empty';
+      let currentNode = this.first;
+      let newTail;
+      while(currentNode) {
+        if(currentNode.next === this.last) newTail = currentNode;
+        currentNode = currentNode.next;
+      }
+      // Copies old tail to return it's value.
+      let copiedOldTail = this.last;
+      // Update current tail to new tail.
+      this.last = newTail;
+      // Point new tail next to null
+      this.last.next = null;
+      return copiedOldTail;
+    }
+    
+    /** traverse(): logs all Node values from Stack. */
+    traverse() {
+      let currentNode = this.first;
+      while(currentNode) {
+        console.log(currentNode.val);
+        currentNode = currentNode.next;
+      }
+    }
+  
+    /** peek(): return the value of the first node in the stack. */
+  
+    peek() {
+      return this.first
+    }
+  
+    /** isEmpty(): return true if the stack is empty, otherwise false */
+  
+    isEmpty() {
+      return this.size === 0 ? true : false; 
+    }
+  }
+  
+  let cars = new Stack();
+  cars.push('Lightning McQueen');
+  cars.push('Mater');
+  cars.push('Doc Hudson');
+  cars.push('Sally Carrera');
+  // cars.traverse();
+  console.log(cars.pop());
+  cars.traverse();
